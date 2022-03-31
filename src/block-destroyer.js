8b4459cbd87b7c715fe928176953e85c9858b22e
Antariksh Patre <antarikshpatre@gmail.com>
2022-03-31 11:50:50 +0530

    Added comments to block-destroyer.js

--- src/block-destroyer.js
+++ src/block-destroyer.js
@@ -1,12 +1,13 @@
+// Let the page load before firing query to select all the button elements.
 window.addEventListener('load', function () {
 
+    // Select all close buttons in the block.
     const messageBlocks = document.querySelectorAll('.button-close');
 
-    console.log(messageBlocks);
-
+    // Add click listener to all the close buttons of the blocks.
     messageBlocks.forEach(msgBlock => {
         msgBlock.addEventListener("click", function(event) {
-            event.target.parentElement.remove();
+            event.target.parentElement.remove();    // Destroy the block by removing their parent element.
         }); 
     });
 
